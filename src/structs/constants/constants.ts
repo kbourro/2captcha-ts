dcfac46e4058f817faed1d7cda5c66417fb24326
Kristofer Bourro <kbourro@gmail.com>
2023-05-25 13:14:31 +0300

    change soft_id

--- src/structs/constants/constants.ts
+++ src/structs/constants/constants.ts
@@ -1,25 +1,25 @@
-const twoCaptchaName = '2captcha'
-const twoCaptchaIn = 'https://2captcha.com/in.php'
-const twoCaptchaRes =  'https://2captcha.com/res.php'
+const twoCaptchaName = "2captcha";
+const twoCaptchaIn = "https://2captcha.com/in.php";
+const twoCaptchaRes = "https://2captcha.com/res.php";
 
-const ruCaptchaName = 'ruCaptcha'
-const ruCaptchaIn = 'https://rucaptcha.com/in.php'
-const ruCaptchaRes =  'https://rucaptcha.com/res.php'
+const ruCaptchaName = "ruCaptcha";
+const ruCaptchaIn = "https://rucaptcha.com/in.php";
+const ruCaptchaRes = "https://rucaptcha.com/res.php";
 
 // TODO: add the ability to specify softid in the constructor of the `Captcha` class
-const softId: number = 3898
+const softId: number = 1771;
 
 const supportedProviders = {
   twoCaptcha: {
     name: twoCaptchaName,
     in: twoCaptchaIn,
-    res: twoCaptchaRes
+    res: twoCaptchaRes,
   },
   ruCaptcha: {
     name: ruCaptchaName,
     in: ruCaptchaIn,
-    res: ruCaptchaRes
-  }
-}
+    res: ruCaptchaRes,
+  },
+};
 
-export { supportedProviders, softId }
+export { supportedProviders, softId };
